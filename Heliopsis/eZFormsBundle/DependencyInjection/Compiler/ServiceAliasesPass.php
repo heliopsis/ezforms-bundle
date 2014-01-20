7c6918dc5f8dc7e153d69ebd1a493abfa7ff71fe
Benjamin Choquet <benjamin.choquet@heliopsis.net>
2014-01-20 18:31:54 +0100

    license header (proprietary for the time being)

--- Heliopsis/eZFormsBundle/DependencyInjection/Compiler/ServiceAliasesPass.php
+++ Heliopsis/eZFormsBundle/DependencyInjection/Compiler/ServiceAliasesPass.php
@@ -1,6 +1,8 @@
 <?php
 /**
- * @author: bchoquet
+ * @author: Benjamin Choquet <bchoquet@heliopsis.net>
+ * @copyright: Copyright (C) 2014 Heliopsis. All rights reserved.
+ * @licence: proprietary
  */
 
 namespace Heliopsis\eZFormsBundle\DependencyInjection\Compiler;
