dbf46ebb5675681dfa98a620b53b706b8ae096a9
Brice Broussolle <bipole3@gmail.com>
2015-04-20 17:43:07 +0200

    Adding ContentType FormProvider & HandlerProvider + adding Chain FormProvider & HandlerProvider

---
+++ Heliopsis/eZFormsBundle/Provider/Form/ChainMap.php
@@ -0,0 +1,62 @@
+<?php
+/**
+ * @copyright: Copyright (C) 2014 Heliopsis. All rights reserved.
+ * @license http://www.gnu.org/licenses/gpl-2.0.txt GNU General Public License v2
+ *
+ * @author: Broussolle Brice <brice.broussolle@outlook.com>
+ */
+
+namespace Heliopsis\eZFormsBundle\Provider\Form;
+
+use eZ\Publish\API\Repository\Values\Content\Location;
+use Heliopsis\eZFormsBundle\Exceptions\UnknownFormException;
+use Heliopsis\eZFormsBundle\Provider\FormProviderInterface;
+
+/**
+ * Class ChainMap
+ * @package Heliopsis\eZFormsBundle\Provider\Form
+ */
+class ChainMap implements FormProviderInterface
+{
+    /**
+     * @var FormProviderInterface[][]
+     */
+    private $providers = array();
+
+    /**
+     * @param FormProviderInterface $formProvider
+     * @param int $priority
+     */
+    public function addProvider( FormProviderInterface $formProvider, $priority = 0 )
+    {
+        if (!array_key_exists($priority, $this->providers)) {
+            $this->providers[$priority] = array();
+        }
+
+        if(!in_array($formProvider,  $this->providers[$priority])) {
+            $this->providers[$priority][] = $formProvider;
+        }
+
+        ksort ( $this->providers );
+    }
+
+    /**
+     * @param Location $location
+     * @return \Symfony\Component\Form\FormInterface
+     * @throws \Heliopsis\eZFormsBundle\Exceptions\UnknownFormException
+     */
+    public function getForm( Location $location )
+    {
+        foreach ($this->providers as $providersPriority) {
+            foreach ($providersPriority as $provider) {
+                try {
+                    return $provider->getForm($location);
+                } catch ( UnknownFormException $e) {
+                    continue;
+                }
+            }
+        }
+
+        throw new UnknownFormException( "No form could be mapped" );
+    }
+}
