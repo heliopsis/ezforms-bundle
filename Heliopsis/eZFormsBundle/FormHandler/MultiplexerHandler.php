87297e42dd29655eac182e4e1ba4c2c9315fbcdd
Benjamin Choquet <benjamin.choquet@heliopsis.net>
2015-04-22 14:49:56 +0200

    Rename ChainHandler to Multiplexer handler to avoid confusion with new chain providers

---
+++ Heliopsis/eZFormsBundle/FormHandler/MultiplexerHandler.php
@@ -0,0 +1,87 @@
+<?php
+/**
+ * MultiplexerHandler Class
+ *
+ * Allows to use multiple handlers one after the other
+ *
+ * @copyright: Copyright (C) 2014 Heliopsis. All rights reserved.
+ * @license http://www.gnu.org/licenses/gpl-2.0.txt GNU General Public License v2
+ */
+
+namespace Heliopsis\eZFormsBundle\FormHandler;
+
+use eZ\Publish\API\Repository\Values\Content\Content;
+use eZ\Publish\API\Repository\Values\Content\Location;
+
+class MultiplexerHandler implements LocationAwareHandlerInterface, ContentAwareHandlerInterface
+{
+    private $handlers = array();
+
+    /**
+     * @param array $handlers
+     */
+    public function __construct( array $handlers = array() )
+    {
+        /**
+         * @var FormHandlerInterface $handler
+         */
+        foreach ( $handlers as $handler )
+        {
+            $this->addHandler( $handler );
+        }
+    }
+
+    /**
+     * Adds a handler at the end of the chain
+     * @param FormHandlerInterface $handler
+     */
+    public function addHandler( FormHandlerInterface $handler )
+    {
+        $this->handlers[] = $handler;
+    }
+
+    /**
+     * @param mixed $data
+     * @return void
+     */
+    public function handle($data)
+    {
+        /**
+         * @var FormHandlerInterface $handler
+         */
+        foreach ( $this->handlers as $handler )
+        {
+            $handler->handle( $data );
+        }
+    }
+
+    /**
+     * Passes location to location aware handlers
+     * @param \eZ\Publish\API\Repository\Values\Content\Location $location
+     */
+    public function setLocation( Location $location )
+    {
+        foreach ( $this->handlers as $handler )
+        {
+            if ( $handler instanceof LocationAwareHandlerInterface )
+            {
+                $handler->setLocation( $location );
+            }
+        }
+    }
+
+    /**
+     * Passes content to content aware handlers
+     * @param \eZ\Publish\API\Repository\Values\Content\Content $content
+     */
+    public function setContent( Content $content )
+    {
+        foreach ( $this->handlers as $handler )
+        {
+            if ( $handler instanceof ContentAwareHandlerInterface )
+            {
+                $handler->setContent( $content );
+            }
+        }
+    }
+}
